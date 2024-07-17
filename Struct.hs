74427cafd6c5b2c328ae91ef683e76cb582a0299
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-07-17 19:47:35 +0200

    Update Struct.hs

--- Struct.hs
+++ Struct.hs
@@ -1,3 +1,32 @@
+-- BSD 3-Clause License
+-- 
+-- Copyright (c) 2024, Pieter Stevens
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 
+-- 1. Redistributions of source code must retain the above copyright notice, this
+--    list of conditions and the following disclaimer.
+--
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+-- 
+-- 3. Neither the name of the copyright holder nor the names of its
+--    contributors may be used to endorse or promote products derived from
+--    this software without specific prior written permission.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 {-# LANGUAGE AllowAmbiguousTypes, FunctionalDependencies, TemplateHaskell #-}
 
 -- Struct.hs used getField from GHC.Records, but defines its own HasField (and thus setField)
@@ -34,7 +63,6 @@ struct structType = do
         fieldP = map (\(n, _, _) -> (mkName $ nameBase n, VarP $ mkName $ nameBase n)) fields
         fieldE = map (\(n, _, _) -> if fieldName /= n then (mkName $ nameBase n, VarE $ mkName $ nameBase n) else (mkName $ nameBase n, VarE f)) fields
 
-        -- *
         -- If you are confused by this. Try runQ [e|34+35|] in GHCI
         -- The point is that this generates the following instance template for all fields:
         --
@@ -42,5 +70,4 @@ struct structType = do
         -- >   hasField r = (\f -> case r of RecordType { .. } -> RecordType { field = f, .. }, r.field)
         --
         -- This way we inherit getField, but define our own setField
-        -- *
         decl = InstanceD Nothing [] (AppT (AppT (AppT (ConT $ mkName "HasField") (LitT (StrTyLit (nameBase fieldName)))) (ConT structType')) fieldType) [FunD (mkName "hasField") [Clause [VarP r] (NormalB (TupE [Just (LamE [VarP f] (CaseE (VarE r) [Match (RecP structType' fieldP) (NormalB (RecConE structType' fieldE)) []])),Just (AppE (AppTypeE (VarE 'GHC.Records.getField) (LitT (StrTyLit (nameBase fieldName)))) (VarE r))])) []]]
